182525d9a666219dc619b185f92eb4bd8487ecb8
鲲鹏 <bravehlp@gmail.com>
2020-01-05 00:31:40 +0800

    增加对于Spring Cloud Hystrix的支持

--- hello-client/src/main/java/com/hello/client/HelloClientApplication.java
+++ hello-client/src/main/java/com/hello/client/HelloClientApplication.java
@@ -2,13 +2,18 @@ package com.hello.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
+// 也可用 @SpringCloudApplication 替代上面3个注解，因为SpringCloudApplication中已经包含了
+//@SpringCloudApplication
 public class HelloClientApplication {
 
 	@Bean
--- hello-client/src/main/java/com/hello/client/web/ConsumerController.java
+++ hello-client/src/main/java/com/hello/client/web/ConsumerController.java
@@ -22,6 +22,7 @@ package com.hello.client.web;
  * ━━━━━━感觉萌萌哒━━━━━━
  */
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -42,11 +43,22 @@ public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "helloFallback")
     @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
     public String helloConsumer(){
         String url = "http://HELLO-SERVICE/hello";
         return restTemplate.getForEntity(url, String.class).getBody();
     }
 
+    /**
+     * Description:
+     * web调用的异常处理方法
+     * @return
+     * @author Alex
+     * @date 2020/1/5 00:30
+     */
+    public String helloFallback(){
+        return "error";
+    }
 
 }
